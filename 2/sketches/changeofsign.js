f652e01cbf1d6df666e221cf6d25ed7befb0f0f5
Jonathan Watson <jonathanw12@outlook.com>
2019-03-03 16:03:33 +0000

    Added two sketches for 2/1

---
+++ 2/sketches/changeofsign.js
@@ -0,0 +1,49 @@
+let t = 0;
+
+const getY = x => (x ** 3) - 0.5 * (x ** 2) + x - 0.5;
+let y;
+
+const update = () => {
+  if (frameRate()) {
+    t += 0.3 / frameRate();
+  }
+
+  y = getY(mouseX / width)
+};
+
+
+
+
+draw = () => {
+  update();
+
+  background('#ff7070');
+
+  noFill();
+  beginShape();
+  for (let x = 0; x < width; x += 1) {
+    vertex(x, height / 2 - getY(x / width) * 100000 / height);
+  }
+  endShape();
+
+  line(0, height / 2, width, height / 2);
+
+  textAlign(CENTER, CENTER);
+  if (y > 0) {
+    fill('red');
+    text('+', mouseX, mouseY, width / 125);
+  } else {
+    fill('blue');
+    text('-', mouseX, mouseY, width / 125);
+  }
+
+  textAlign(LEFT, BASELINE);
+  noFill();
+  circle(width / 2, height / 2, width / 125);
+  fill('black');
+  text('0.5', width / 2, height / 2);
+
+  text('x', width / 100, height / 2);
+
+  text(`y = ${y.toFixed(2)}`, width / 100, width / 40);
+};
---
+++ 2/sketches/roots.js
@@ -0,0 +1,42 @@
+let t = 0;
+const roots = new Array(3);
+
+const getY = x => roots.map(root => x - root * width).reduce((a, b) => a * b);
+
+const update = () => {
+  if (frameRate()) {
+    t += 0.3 / frameRate();
+  }
+
+  for (let i = 0; i < roots.length; i += 1) {
+    roots[i] = noise(t + 1000 * i) / roots.length + i / roots.length;
+  }
+
+  roots.sort((a, b) => a - b);
+};
+
+draw = () => {
+  update();
+
+  background('#ff7070');
+
+  noFill();
+  beginShape();
+  for (let x = 0; x < width; x += 1) {
+    vertex(x, height / 2 - getY(x) / 125 / height);
+  }
+  endShape();
+
+  line(0, height / 2, width, height / 2);
+
+  roots.forEach((root) => {
+    noFill();
+    circle(root * width, height / 2, width / 125);
+    fill('black');
+    text(root.toFixed(2), root * width, height / 2);
+  });
+
+  text('x', width / 100, height / 2);
+
+  text(`y = ${roots.map(root => (`(x - ${root.toFixed(2)})`)).join('')}`, width / 100, width / 40);
+};
