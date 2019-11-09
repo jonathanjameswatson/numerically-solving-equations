e95624fcec77a7203cfd7f87b598befe4ba759c1
Jonathan Watson <jonathanw12@outlook.com>
2019-11-09 21:49:30 +0000

    Added markdown support

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,6 +50,11 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config, ctx) {
+      config.module.rules.push({
+        test: /\.md$/i,
+        loader: 'raw-loader'
+      })
+    }
   }
 }
