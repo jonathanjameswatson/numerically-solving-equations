1b19980f0eef11a934cd34926c88783b26059b46
Jonathan Watson <jonathanw12@outlook.com>
2019-11-11 13:05:35 +0000

    Added dynamic page

---
+++ pages/_page.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="section">
+    <div class="content">
+      <v-runtime-template :template="content" />
+    </div>
+  </section>
+</template>
+
+<script>
+import VRuntimeTemplate from 'v-runtime-template'
+import Card from '~/components/Card'
+
+export default {
+  components: {
+    VRuntimeTemplate,
+    Card // eslint-disable-line vue/no-unused-components
+  },
+  async asyncData({ params }) {
+    const Content = await import(`~/assets/contents/${params.page}.md`)
+    return { content: Content.default }
+  }
+}
+</script>
