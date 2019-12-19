c60a05ab1079bea63e31d5b4b3fc9ac8b17062f3
Jonathan Watson <jonathanw12@outlook.com>
2019-12-19 13:41:47 +0000

    Changed error page

---
+++ layouts/error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="content">
+    <h1>Error {{ error.statusCode }}</h1>
+    <p v-if="error.statusCode === 404">
+      Page not found
+    </p>
+    <p v-else>
+      An error occurred
+    </p>
+    <b-button tag="nuxt-link" type="is-link" to="/">
+      Return to home
+    </b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error']
+}
+</script>
