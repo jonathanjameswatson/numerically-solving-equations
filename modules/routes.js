ac9751ba730b512211dd4c6205940b5f9bef6345
Jonathan Watson <jonathanw12@outlook.com>
2019-12-31 16:04:38 +0000

    Fixed generate

--- modules/routes.js
+++ modules/routes.js
@@ -5,6 +5,6 @@ export default async function routes() {
   const pageNames = await pagesGenerator()
   const methodNames = Object.getOwnPropertyNames(utilities.methods)
   const routes = pageNames.map((page) => `/${page}`)
-  routes.push(methodNames.map((method) => `/calculator/${method}`))
+  routes.push(...methodNames.map((method) => `/calculator/${method}`))
   this.options.generate.routes = this.options.generate.routes.concat(routes)
 }
